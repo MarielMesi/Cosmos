616ab0ec10fd276e932e207a411c3d5b995a842b
MarielMesi <100303309+MarielMesi@users.noreply.github.com>
2022-03-01 13:29:32 +0200

    InterGalactic assets.
    Assets including health system.

---
+++ Assets/HealthController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public int playerHealth;
+
+    [SerializeField] private Image[] hearts;
+
+    private void start()
+    {
+        UpdateHealth();
+    }
+
+    public void UpdateHealth()
+    {
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerHealth)
+            {
+                hearts[i].color = Color.red;
+            }
+            else
+            {
+                hearts[i].color = Color.black;
+            }
+        }
+    }
+}

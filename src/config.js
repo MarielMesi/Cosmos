57acdd1144e397a9d85b3c1ff9b52e29df659a74
MarielMesi <100303309+MarielMesi@users.noreply.github.com>
2022-05-06 11:23:48 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,21 @@
+const description = "Cosmos spaceships NFTs";
+const baseUri = "COSMOS";
+const layersOrder = [
+  { name: "base" },
+  { name: "cockpits" },
+  { name: "wings" }
+];
+const format = {
+  width: 840,
+  height: 859,
+};
+const uniqueDnaTorrance = 0;
+const editionSize = 0;
+module.exports = {
+  layersOrder,
+  format,
+  editionSize,
+  baseUri,
+  description,
+  uniqueDnaTorrance,
+};
